39d4d9434824ef7489b44465457e4fc5be26aeb3
Skip Wyatt <childofthewired@users.noreply.github.com>
2014-10-09 15:59:55 -0400

    second commit, adding script builder

--- scriptbuilder.sh
+++ scriptbuilder.sh
@@ -2,7 +2,7 @@
 #When executed, this will build a an executable file with the correct header for a bash script with date created etcetera
 
 
-read -p "Enter the same of the script:" scriptname
+read -p "Enter the name of the script:" scriptname
 
 
 touch $scriptname
