6c99ce383614bd08c827502cc4b0bbc3550b464d
Skip Wyatt <childofthewired@users.noreply.github.com>
2014-10-09 15:45:12 -0400

    second commit, adding script builder

---
+++ scriptbuilder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#When executed, this will build a an executable file with the correct header for a bash script with date created etcetera
+
+
+read -p "Enter the same of the script:" scriptname
+
+
+touch $scriptname
+
+echo "#!/bin/bash" >> $scriptname
+echo "#This file was created on $(date) by Skip Wyatt" >> $scriptname
+echo "#" >> $scriptname
+echo "#" >> $scriptname
+
+chmod +x $scriptname
+
+
